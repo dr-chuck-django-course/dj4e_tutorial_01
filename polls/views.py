bc38dd12fdfdcee4b9e009d86a0a89ca1f59b319
Viktor Gordeev <gordeewiktor@gmail.com>
2025-07-20 17:20:57 +0700

    added af38aca9 to the index

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. e2182fb0 is the polls index.")
+    return HttpResponse("Hello, world. af38aca9 is the polls index.")
